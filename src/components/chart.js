b126be69c26a8004c2a438a9f36cb0ca4d4da790
wilteadus <wilteadus@gmail.com>
2017-07-23 21:00:02 -0400

    first commit

---
+++ src/components/chart.js
@@ -0,0 +1,18 @@
+import _ from 'lodash'
+import React from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines'
+
+export default (props) => {
+    function average(data) {
+        return _.round(_.sum(data)/data.length);
+    }
+    return(
+        <div>
+            <Sparklines height={120} width={160} data={props.data}>
+                <SparklinesLine color={props.color}/>
+                <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+            <div>{average(props.data)} {props.unit}</div>
+        </div>
+        )
+}
